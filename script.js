ffac7348a75241cc4955d25e88d3d29ddaa81b34
Alanov <alanov_dev@proton.me>
2022-08-23 16:09:47 -0300

    Added a function to play a round

--- script.js
+++ script.js
@@ -14,9 +14,31 @@ function getComputerChoice() {
         return "SCISSORS";
     }
 }
-function playerSelection() {
+function playerSelect() {
     const getString = window.prompt("Select: Rock, Paper or Scissors?");   
     const playerChoice = getString.toUpperCase();
     
     return playerChoice;
 } 
+function playRound(computerSelection, playerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = playerSelect();
+
+    console.log(`computer selection: ${computerSelection}`);
+    console.log(`player selection: ${playerSelection}`);
+    if(computerSelection === playerSelection) {
+        return "It's a Draw!";
+    } else if(computerSelection === "PAPER" && playerSelection === "ROCK") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if(computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+}
